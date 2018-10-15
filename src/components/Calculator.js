8dfa01a52d1565a9a9c154b395d3981f3e8cb8df
Dileep Rajput <dileep_rajput@yahoo.com>
2018-10-14 20:48:56 -0400

    Add operator buttons to Calculator component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -14,13 +14,23 @@ class Calculator extends Component {
     this.handleNumberClick = this.handleNumberClick.bind(this);
     this.handleDecimalClick = this.handleDecimalClick.bind(this);
     this.handleClearClick = this.handleClearClick.bind(this);
+    this.handleOperatorClick = this.handleOperatorClick.bind(this);
   }
 
   handleNumberClick(event) {
     const current = this.state.current;
 
+    if(/(\+|-|\/|\*)/.test(current)) {
+      this.setState({
+        calculation: this.state.calculation + current,
+        current: event.target.value
+      });
+      return;
+    }
+
     if(current.length == 0 || /(\d|\d\.\d|\.)/.test(current)) {
       this.setState({current: current + event.target.value});
+      return;
     }
 
   }
@@ -35,11 +45,41 @@ class Calculator extends Component {
     this.setState({calculation: '', current: ''});
   }
 
+  handleOperatorClick(event) {
+    const current = this.state.current;
+
+    if (/(\d|\d\.\d)/.test(current)) {
+      this.setState({
+        calculation: this.state.calculation + current,
+        current: event.target.value
+      });
+      return;
+    }
+
+  }
+
   render() {
     return (
       <div>
         <CalcDisplay calculation={this.state.calculation} current={this.state.current}/>
         <CalcButton id="clear" value="AC" onClick={this.handleClearClick} />
+        {
+          [
+            {id: 'add', value: '+'},
+            {id: 'subtract', value: '-'},
+            {id: 'multiply', value: '*'},
+            {id: 'divide', value: '/'}
+          ].map(
+            (operator) => {
+              return <CalcButton
+                      key={operator.id}
+                      id={operator.id}
+                      value={operator.value}
+                      onClick={this.handleOperatorClick}
+                     />;
+            }
+          )
+        }
         <div id="numpad">
           {
             ['nine', 'eight', 'seven', 'six', 'five', 'four', 'three', 'two', 'one', 'zero'].map(
