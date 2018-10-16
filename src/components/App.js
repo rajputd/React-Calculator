ce076f91209389c73779cddad79f2ec0898e22e0
Dileep Rajput <dileep_rajput@yahoo.com>
2018-10-15 22:19:16 -0400

    Add headers and center content

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import Calculator from './Calculator.js';
+import '../css/App.css';
 
 class App extends Component{
   render() {
     return (
-      <Calculator />
+      <div id="App">
+        <h1 class="center">React Calculator</h1>
+        <Calculator />
+        <p class="center">A project by Dileep Rajput, inspired by Peter Weinberg</p>
+      </div>
     );
   }
 }
